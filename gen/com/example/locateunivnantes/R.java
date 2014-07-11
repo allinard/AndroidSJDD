19103afc42b2af07981f607f5faf9850562b0f2b
allinard <al.linard@gmail.com>
2014-07-11 11:55:47 +0200

    bins

--- gen/com/example/locateunivnantes/R.java
+++ gen/com/example/locateunivnantes/R.java
@@ -35,6 +35,11 @@ public final class R {
          Customize dimensions originally defined in res/values/dimens.xml (such as
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
+ Default screen margins, per the Android Design guidelines. 
+ Default screen margins, per the Android Design guidelines. 
+ Default screen margins, per the Android Design guidelines. 
+ Default screen margins, per the Android Design guidelines. 
+ Default screen margins, per the Android Design guidelines. 
          */
         public static final int activity_horizontal_margin=0x7f050000;
         public static final int activity_vertical_margin=0x7f050001;
@@ -74,70 +79,72 @@ public final class R {
     }
     public static final class id {
         public static final int activity_itineraire=0x7f060014;
-        public static final int activity_itineraire1=0x7f060015;
-        public static final int activity_itineraire2=0x7f06001b;
+        public static final int activity_itineraire2=0x7f060015;
         /**  Messages IDs 
+ Messages IDs 
+ Messages IDs 
+ Messages IDs 
+ Messages IDs 
+ Messages IDs 
          */
         public static final int auto_focus=0x7f060000;
-        public static final int barcode_image_view=0x7f060023;
+        public static final int barcode_image_view=0x7f06001e;
         public static final int btnAller=0x7f06000b;
-        public static final int btnChangeBat=0x7f06001a;
-        public static final int btnLogin=0x7f06001e;
+        public static final int btnChangeBat=0x7f060016;
+        public static final int btnLogin=0x7f060019;
         public static final int btnOK=0x7f06000e;
         public static final int btnOu=0x7f06000c;
         public static final int btnQRCode=0x7f060012;
         public static final int btnSalle=0x7f060013;
-        public static final int contents_text_view=0x7f06002c;
+        public static final int contents_text_view=0x7f060027;
         public static final int custom_font=0x7f060007;
         public static final int decode=0x7f060001;
         public static final int decode_failed=0x7f060002;
         public static final int decode_succeeded=0x7f060003;
-        public static final int editTextLogin=0x7f06001c;
-        public static final int editTextPassWd=0x7f06001d;
-        public static final int encode_view=0x7f06002e;
-        public static final int format_text_view=0x7f060025;
-        public static final int format_text_view_label=0x7f060024;
+        public static final int editTextLogin=0x7f060017;
+        public static final int editTextPassWd=0x7f060018;
+        public static final int encode_view=0x7f060029;
+        public static final int format_text_view=0x7f060020;
+        public static final int format_text_view_label=0x7f06001f;
         public static final int imageView1=0x7f060009;
-        public static final int imageView2=0x7f06001f;
-        public static final int image_view=0x7f06002f;
-        public static final int imgBat=0x7f060032;
-        public static final int item1=0x7f060036;
-        public static final int item2=0x7f060037;
-        public static final int item3=0x7f060038;
-        public static final int item4=0x7f060039;
-        public static final int item5=0x7f06003a;
-        public static final int item6=0x7f06003e;
-        public static final int item7=0x7f06003b;
-        public static final int item8=0x7f06003c;
-        public static final int item9=0x7f06003d;
-        public static final int layerBat=0x7f060033;
-        public static final int layerPointArrivee=0x7f060035;
-        public static final int layerPointDepart=0x7f060034;
-        public static final int lblListHeader=0x7f060030;
-        public static final int lblListItem=0x7f060031;
+        public static final int imageView2=0x7f06001a;
+        public static final int image_view=0x7f06002a;
+        public static final int imgBat=0x7f06002d;
+        public static final int item1=0x7f060031;
+        public static final int item10=0x7f060039;
+        public static final int item11=0x7f06003a;
+        public static final int item2=0x7f060032;
+        public static final int item3=0x7f060033;
+        public static final int item4=0x7f060034;
+        public static final int item5=0x7f060035;
+        public static final int item6=0x7f06003b;
+        public static final int item7=0x7f060036;
+        public static final int item8=0x7f060037;
+        public static final int item9=0x7f060038;
+        public static final int layerBat=0x7f06002e;
+        public static final int layerPointArrivee=0x7f060030;
+        public static final int layerPointDepart=0x7f06002f;
+        public static final int lblListHeader=0x7f06002b;
+        public static final int lblListItem=0x7f06002c;
         public static final int listViewInfo=0x7f060008;
         public static final int lvExp=0x7f06000d;
-        public static final int meta_text_view=0x7f06002b;
-        public static final int meta_text_view_label=0x7f06002a;
-        public static final int preview_view=0x7f060020;
+        public static final int meta_text_view=0x7f060026;
+        public static final int meta_text_view_label=0x7f060025;
+        public static final int preview_view=0x7f06001b;
         public static final int quit=0x7f060004;
         public static final int restart_preview=0x7f060005;
-        public static final int result_view=0x7f060022;
+        public static final int result_view=0x7f06001d;
         public static final int return_scan_result=0x7f060006;
         public static final int salleDestination=0x7f060011;
-        public static final int status_view=0x7f06002d;
+        public static final int status_view=0x7f060028;
         public static final int textView=0x7f06000a;
         public static final int textView1=0x7f06000f;
-        public static final int textView2=0x7f060016;
         public static final int textView3=0x7f060010;
-        public static final int textView4=0x7f060018;
-        public static final int time_text_view=0x7f060029;
-        public static final int time_text_view_label=0x7f060028;
-        public static final int txtDestination=0x7f060017;
-        public static final int txtOrigine=0x7f060019;
-        public static final int type_text_view=0x7f060027;
-        public static final int type_text_view_label=0x7f060026;
-        public static final int viewfinder_view=0x7f060021;
+        public static final int time_text_view=0x7f060024;
+        public static final int time_text_view_label=0x7f060023;
+        public static final int type_text_view=0x7f060022;
+        public static final int type_text_view_label=0x7f060021;
+        public static final int viewfinder_view=0x7f06001c;
     }
     public static final class layout {
         public static final int activity_aide=0x7f030000;
@@ -160,40 +167,58 @@ public final class R {
         public static final int main=0x7f090000;
     }
     public static final class string {
-        public static final int action_settings=0x7f070001;
-        public static final int app_name=0x7f070000;
-        public static final int barcode_image=0x7f070021;
-        public static final int capture_name=0x7f070003;
-        public static final int contents_contact=0x7f070006;
-        public static final int contents_email=0x7f070007;
-        public static final int contents_location=0x7f070008;
-        public static final int contents_phone=0x7f070009;
-        public static final int contents_sms=0x7f07000a;
-        public static final int contents_text=0x7f07000b;
-        public static final int decoder_name=0x7f070005;
-        public static final int encoder_name=0x7f070004;
-        public static final int hello_world=0x7f070002;
-        public static final int msg_default_contents=0x7f07000c;
-        public static final int msg_default_format=0x7f07000d;
-        public static final int msg_default_meta=0x7f07000e;
-        public static final int msg_default_mms_subject=0x7f07000f;
-        public static final int msg_default_status=0x7f070010;
-        public static final int msg_default_time=0x7f070011;
-        public static final int msg_default_type=0x7f070012;
-        public static final int result_address_book=0x7f070013;
-        public static final int result_calendar=0x7f070014;
-        public static final int result_email_address=0x7f070015;
-        public static final int result_geo=0x7f070016;
-        public static final int result_isbn=0x7f070017;
-        public static final int result_product=0x7f070018;
-        public static final int result_sms=0x7f070019;
-        public static final int result_tel=0x7f07001a;
-        public static final int result_text=0x7f07001b;
-        public static final int result_uri=0x7f07001c;
-        public static final int result_wifi=0x7f07001d;
-        public static final int wifi_changing_network=0x7f07001e;
-        public static final int wifi_ssid_label=0x7f07001f;
-        public static final int wifi_type_label=0x7f070020;
+        public static final int action_settings=0x7f070013;
+        public static final int app_name=0x7f070012;
+        public static final int barcode_image=0x7f070033;
+        public static final int capture_name=0x7f070015;
+        public static final int contents_contact=0x7f070018;
+        public static final int contents_email=0x7f070019;
+        public static final int contents_location=0x7f07001a;
+        public static final int contents_phone=0x7f07001b;
+        public static final int contents_sms=0x7f07001c;
+        public static final int contents_text=0x7f07001d;
+        public static final int couleurs=0x7f07000e;
+        public static final int decoder_name=0x7f070017;
+        public static final int encoder_name=0x7f070016;
+        public static final int hello_world=0x7f070014;
+        public static final int home=0x7f07000b;
+        public static final int id=0x7f070008;
+        public static final int info_dest=0x7f070004;
+        public static final int informations=0x7f070010;
+        public static final int langues=0x7f070011;
+        public static final int localisez=0x7f070005;
+        public static final int login=0x7f07000a;
+        public static final int mdp=0x7f070009;
+        public static final int msg_default_contents=0x7f07001e;
+        public static final int msg_default_format=0x7f07001f;
+        public static final int msg_default_meta=0x7f070020;
+        public static final int msg_default_mms_subject=0x7f070021;
+        public static final int msg_default_status=0x7f070022;
+        public static final int msg_default_time=0x7f070023;
+        public static final int msg_default_type=0x7f070024;
+        public static final int prefs=0x7f07000c;
+        public static final int prefs_appli=0x7f07000d;
+        public static final int qr=0x7f070007;
+        public static final int result_address_book=0x7f070025;
+        public static final int result_calendar=0x7f070026;
+        public static final int result_email_address=0x7f070027;
+        public static final int result_geo=0x7f070028;
+        public static final int result_isbn=0x7f070029;
+        public static final int result_product=0x7f07002a;
+        public static final int result_sms=0x7f07002b;
+        public static final int result_tel=0x7f07002c;
+        public static final int result_text=0x7f07002d;
+        public static final int result_uri=0x7f07002e;
+        public static final int result_wifi=0x7f07002f;
+        public static final int salle=0x7f070006;
+        public static final int scan_qr_code=0x7f070003;
+        public static final int select_localisation=0x7f070002;
+        public static final int select_mode=0x7f070000;
+        public static final int select_nav=0x7f070001;
+        public static final int version_handicap=0x7f07000f;
+        public static final int wifi_changing_network=0x7f070030;
+        public static final int wifi_ssid_label=0x7f070031;
+        public static final int wifi_type_label=0x7f070032;
     }
     public static final class style {
         /** 
@@ -209,16 +234,83 @@ public final class R {
         Base application theme for API 11+. This theme completely replaces
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
     
- API 11 theme customizations can go here. 
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
 
         Base application theme for API 14+. This theme completely replaces
         AppBaseTheme from BOTH res/values/styles.xml and
         res/values-v11/styles.xml on API 14+ devices.
     
- API 14 theme customizations can go here. 
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
+
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
+
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
+
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
+
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
+
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
          */
         public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
+ Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
+ Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
+ Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
+ Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
+ Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
+ Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
+ Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f080001;
